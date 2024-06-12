2e00fb8132b40fc34aa08193a72dab86b9ad32fc
Matthew Truesdale <157014037+ThEmeralDuke@users.noreply.github.com>
2024-06-12 21:28:41 +0100

    Delete David_Bot directory

--- David_Bot/restart.py
+++
@@ -1,5 +0,0 @@
-from subprocess import Popen, PIPE
-print("Restart program active")
-process = Popen(['swfdump', '/home/ubuntu/python/Bots/test/bot.py', '-d'], stdout=PIPE, stderr=PIPE)
-stdout, stderr = process.communicate()
-exit()
