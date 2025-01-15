eb0a29695fb17974052a8cb70166b8aa05f0e912
ThEmeralDuke <mattytruesdale@gmail.com>
2025-01-15 19:02:20 +0000

    test

--- bot.py
+++ bot.py
@@ -610,5 +610,6 @@ def insertion_sort_2d_Descending(arr, col_index):
 Warningsystemthread= threading.Thread(target=Warningsystem)
 Warningsystemthread.start()
 load_dotenv()
+print(os.getenv("DAVID_KEY"))
 bot.run(str(os.getenv("DAVID_KEY")))
 
