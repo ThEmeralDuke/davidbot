692fd34b24138b5da684a8fc1944331c6d0f18b5
ThEmeralDuke <mattytruesdale@gmail.com>
2025-05-16 17:20:01 +0100

    NO ENTER

--- coggers/admin.py
+++ coggers/admin.py
@@ -116,7 +116,7 @@ class admin(commands.Cog):
     @commands.has_role(Adminrole)
     async def update(self ,ctx):
         print("Update started...")
-        Test= subprocess.run(["sudo","git","pull","ENTER"])
+        Test= subprocess.run(["sudo","git","pull"])
         print(test)
         print("Update done")
         await ctx.send("Bot Updated. if this is a cog update please !reload the cog. if not reboot the program")
