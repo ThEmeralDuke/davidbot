1c57dfa653c2498c1829bb2d9f19987e97dc7d17
ThEmeralDuke <mattytruesdale@gmail.com>
2025-05-18 18:34:57 +0100

    redid it

--- coggers/hypixel.py
+++ coggers/hypixel.py
@@ -71,7 +71,7 @@ class hypixel(commands.Cog):
         minutes_left %= MINUTES_PER_IG_HOUR
 
         IG_minute = int(minutes_left // MINUTES_PER_IG_MINUTE)
-        print(f"Year: {IG_year}, Month: {IG_month + 1}, Day: {IG_day + 1}, Hour: {IG_hour}, Minute: {IG_minute}")
+        print(f"Year: {IG_year+1}, Month: {IG_month + 1}, Day: {IG_day + 1}, Hour: {IG_hour+1}, Minute: {IG_minute}")
         #embed=discord.Embed(title="Hypixel Calander", color=0x808080)
         #embed.set_thumbnail(url="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fyt3.ggpht.com%2F-G0UwZhD1hRI%2FAAAAAAAAAAI%2FAAAAAAAAAAA%2FQ5bg4hzv6C0%2Fs900-c-k-no-mo-rj-c0xffffff%2Fphoto.jpg&f=1&nofb=1&ipt=f801051e8936792627a8168f1b1608ee768a1502e30ebdd4407b443eab91cc49")
         #embed.add_field(name="Current Hypixel time", value="(date+time+ToD)", inline=True)
