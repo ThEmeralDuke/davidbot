18c9172a3d2fad9ec0f8f4d0f62dc0690388b3f8
ThEmeralDuke <mattytruesdale@gmail.com>
2025-05-18 14:23:51 +0100

    new cogs

---
+++ coggers/hypixel.py
@@ -0,0 +1,53 @@
+#Loading Log files
+filepath= "."
+Errorlog= filepath+"/ImportantTxtFiles/Logs/Error.log"
+def LogError(Level,Reason):
+    with open (Errorlog, "a") as log:
+            currenttime= str(time.strftime("%Y-%m-%D %H:%M:%S", time.localtime()))
+            log.write(f"{currenttime}    ({Level}) {Reason}\n")
+    log.close()
+
+
+#loading libraries
+import os
+import os.path
+from dotenv import *
+import discord
+from discord import *
+from discord.ext import commands
+from discord.utils import *
+import time
+import csv
+import psutil
+import random
+import threading
+import subprocess
+import keyboard
+import json
+import asyncio
+
+#stuff
+botrole= []
+Adminrole= []
+person= ""
+Generallog= filepath+"/ImportantTxtFiles/Logs/General.log"
+LocalFilepath= "/home/server/" #Change this to your local devices filepath
+#load roles (potentially merge this with the settings file)
+with open (filepath+"/ImportantTxtFiles/important.csv", "r") as info:
+    reader= csv.reader(info)
+    for row in reader:
+        botrole= row[0]
+        Adminrole=row[1]
+info.close()
+
+
+class hypixel(commands.Cog):
+    def __init__(self, bot):
+        self.bot= bot
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+         print("hypixel.py is ready")
+
+async def setup(bot):
+      await bot.add_cog(hypixel(bot))
