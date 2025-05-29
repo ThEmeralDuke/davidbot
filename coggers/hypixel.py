07b5c9ed28eeacda546f001db5c8c78beb9fa06f
ThEmeralDuke <mattytruesdale@gmail.com>
2025-05-29 15:02:47 +0100

    try this

--- coggers/hypixel.py
+++ coggers/hypixel.py
@@ -37,58 +37,32 @@ skyblockstart = datetime.strptime("2019-06-11 17:55:00", "%Y-%m-%d %H:%M:%S").re
 
 
 class hypixel(commands.Cog):
-    
     def __init__(self, bot):
         self.bot = bot
-        self.first=True
-        self.channel = self.bot.get_channel(1372924740993548360)
-        self.calenderincrement = tasks.loop(seconds=4.165)(self.update_calendar)
+        self.channel = None
+        self.calendar_message = None
+        self.first = True
+        self.season = "Spring"  # Placeholder; you can add logic to set this based on date
         self.IG_year = 0
         self.IG_month = 0
         self.IG_day = 0
         self.IG_hour = 0
         self.IG_minute = 0
-        self.season= None
-    i=True
-    async def calenderinit(self):
-        while True:
-            utc_now = datetime.now(timezone.utc)
-            skyblockstart = datetime.strptime("2019-06-11 17:55:00", "%Y-%m-%d %H:%M:%S").replace(tzinfo=timezone.utc)
-            secondssincestart = (utc_now - skyblockstart).total_seconds()
-            minutessincestart = int(secondssincestart // 60)
+        self.IG_minutetenth = "00"
 
-            # Skyblock time constants
-            MINUTES_PER_IG_MINUTE = 0.01388
-            MINUTES_PER_IG_HOUR = 0.8333
-            MINUTES_PER_IG_DAY = 20
-            MINUTES_PER_IG_MONTH = 620
-            MINUTES_PER_IG_YEAR = 7440
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print("HypixelCalendar cog is ready")
+        self.channel = self.bot.get_channel(1372924740993548360)  # Replace with actual ID
+        await self.channel.purge()
+        await self.update_calendar()
+        self.update_calendar_loop.start()
 
-            IG_year, remainder = divmod(minutessincestart, MINUTES_PER_IG_YEAR)
-            IG_month, remainder = divmod(remainder, MINUTES_PER_IG_MONTH)
-            IG_day, remainder = divmod(remainder, MINUTES_PER_IG_DAY)
-            IG_hour, remainder = divmod(remainder, MINUTES_PER_IG_HOUR)
-            IG_minute, _ = divmod(remainder, MINUTES_PER_IG_MINUTE)
-
-            self.IG_year = IG_year + 1
-            self.IG_month = IG_month + 1
-            self.IG_day = IG_day + 1
-            self.IG_hour = int(IG_hour)
-            self.IG_minute = int(IG_minute)
-            self.IG_minutetenth = f"{self.IG_minute:02}"
-            if self.IG_minute % 5 == 0:
-                print(f"Initial IG time: {self.IG_year}-{self.IG_month}-{self.IG_day} {self.IG_hour}:{self.IG_minutetenth}")
-                if not self.calenderincrement.is_running():
-                    self.calenderincrement.start()
-                break
-
-
-    async def update_calendar(self):
-        # Recalculate current IG time
+    def get_ig_time(self):
         utc_now = datetime.now(timezone.utc)
-        skyblockstart = datetime.strptime("2019-06-11 17:55:00", "%Y-%m-%d %H:%M:%S").replace(tzinfo=timezone.utc)
-        secondssincestart = (utc_now - skyblockstart).total_seconds()
-        minutessincestart = int(secondssincestart // 60)
+        skyblock_start = datetime.strptime("2019-06-11 17:55:00", "%Y-%m-%d %H:%M:%S").replace(tzinfo=timezone.utc)
+        seconds_since_start = (utc_now - skyblock_start).total_seconds()
+        total_real_minutes = seconds_since_start / 60
 
         MINUTES_PER_IG_MINUTE = 0.01388
         MINUTES_PER_IG_HOUR = 0.8333
@@ -96,43 +70,39 @@ class hypixel(commands.Cog):
         MINUTES_PER_IG_MONTH = 620
         MINUTES_PER_IG_YEAR = 7440
 
-        IG_year, remainder = divmod(minutessincestart, MINUTES_PER_IG_YEAR)
+        IG_year, remainder = divmod(total_real_minutes, MINUTES_PER_IG_YEAR)
         IG_month, remainder = divmod(remainder, MINUTES_PER_IG_MONTH)
         IG_day, remainder = divmod(remainder, MINUTES_PER_IG_DAY)
         IG_hour, remainder = divmod(remainder, MINUTES_PER_IG_HOUR)
         IG_minute, _ = divmod(remainder, MINUTES_PER_IG_MINUTE)
 
-        self.IG_year = IG_year + 1
-        self.IG_month = IG_month + 1
-        self.IG_day = IG_day + 1
+        self.IG_year = int(IG_year) + 1
+        self.IG_month = int(IG_month) + 1
+        self.IG_day = int(IG_day) + 1
         self.IG_hour = int(IG_hour)
         self.IG_minute = int(IG_minute)
         self.IG_minutetenth = f"{self.IG_minute:02}"
-        if self.first== True:
-            calanderembed=discord.Embed(title="Hypixel Calander", color=0x808080)
-            calanderembed.set_thumbnail(url="attachment://ImportantTxtFiles/HypixelLogo.png")
-            calanderembed.add_field(name="Current Hypixel time", value=(f"{self.IG_year}/{self.IG_month}/{self.IG_day}, {int(self.IG_hour)}:{self.IG_minutetenth}"), inline=True)
-            calanderembed.add_field(name="Cuurent Season", value=self.season, inline=True)
-            calanderembed.add_field(name="", value="", inline=False)
-            calanderembed.add_field(name="Current Major Events", value="CurrentEvents", inline=True)
-            calanderembed.add_field(name="Major Events soon", value="EventsSoon", inline=True)
+
+    async def update_calendar(self):
+        self.get_ig_time()
+        calanderembed = discord.Embed(title="Hypixel Calander", color=0x808080)
+        calanderembed.set_thumbnail(url="attachment://ImportantTxtFiles/HypixelLogo.png")
+        calanderembed.add_field(name="Current Hypixel time", value=(f"{self.IG_year}/{self.IG_month}/{self.IG_day}, {int(self.IG_hour)}:{self.IG_minutetenth}"), inline=True)
+        calanderembed.add_field(name="Cuurent Season", value=self.season, inline=True)
+        calanderembed.add_field(name="", value="", inline=False)
+        calanderembed.add_field(name="Current Major Events", value="CurrentEvents", inline=True)
+        calanderembed.add_field(name="Major Events soon", value="EventsSoon", inline=True)
+
+        if self.first:
             self.calendar_message = await self.channel.send(embed=calanderembed)
-            self.first=False
+            self.first = False
         else:
-            calanderembed=discord.Embed(title="Hypixel Calander", color=0x808080)
-            calanderembed.set_thumbnail(url="attachment://ImportantTxtFiles/HypixelLogo.png")
-            calanderembed.add_field(name="Current Hypixel time", value=(f"{self.IG_year}/{self.IG_month}/{self.IG_day}, {int(self.IG_hour)}:{self.IG_minutetenth}"), inline=True)
-            calanderembed.add_field(name="Cuurent Season", value=self.season, inline=True)
-            calanderembed.add_field(name="", value="", inline=False)
-            calanderembed.add_field(name="Current Major Events", value="CurrentEvents", inline=True)
-            calanderembed.add_field(name="Major Events soon", value="EventsSoon", inline=True)
             await self.calendar_message.edit(embed=calanderembed)
-    @commands.Cog.listener()
-    async def on_ready(self):
-        print("hypixel.py is ready")
-        self.channel = self.bot.get_channel(1372924740993548360)
-        await self.channel.purge()
-        asyncio.create_task(self.calenderinit())
+
+    @tasks.loop(seconds=4.165)
+    async def update_calendar_loop(self):
+        await self.update_calendar()
+
 
         
     
