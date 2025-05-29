bde67b08ef5135db5ef37b7e40a0c4db53879af5
ThEmeralDuke <mattytruesdale@gmail.com>
2025-05-29 14:41:31 +0100

    try this

--- coggers/hypixel.py
+++ coggers/hypixel.py
@@ -37,56 +37,77 @@ skyblockstart = datetime.strptime("2019-06-11 17:55:00", "%Y-%m-%d %H:%M:%S").re
 
 
 class hypixel(commands.Cog):
-    IG_year = 0
-    IG_month = 0
-    IG_day = 0
-    IG_hour = 0
-    IG_minute = 0
+    
     def __init__(self, bot):
         self.bot = bot
         self.first=True
         self.channel = self.bot.get_channel(1372924740993548360)
-    @tasks.loop(seconds=4.165)
-    async def calenderincrement(self):
-        self.IG_minute += 5
-        if self.IG_minute >= 65:
-            self.IG_minute -= 60
-            self.IG_hour += 1
-        if self.IG_hour >= 24:
-            self.IG_hour = 0
-            self.IG_day += 1
-        if self.IG_day > 31:
-            self.IG_day = 1
-            self.IG_month += 1
-        if self.IG_month > 12:
-            self.IG_month = 1
-            self.IG_year += 1
-        self.IG_minutetenth = f"{self.IG_minute-5:02}"
-        if self.IG_month== 1:
-            self.season= "Early Spring"
-        elif self.IG_month== 2:
-            self.season= "Spring"
-        elif self.IG_month== 3:
-            self.season= "Late Spring"
-        elif self.IG_month== 4:
-            self.season= "Early Summer"
-        elif self.IG_month== 5:
-            self.season= "Summer"
-        elif self.IG_month== 6:
-            self.season= "Late Summer"
-        elif self.IG_month== 7:
-            self.season= "Early Autumn"
-        elif self.IG_month== 8:
-            self.season= "Autumn"
-        elif self.IG_month== 9:
-            self.season= "Late Autumn"
-        elif self.IG_month== 10:
-            self.season= "Early Winter"
-        elif self.IG_month== 11:
-            self.season= "Winter"
-        elif self.IG_month== 12:
-            self.season= "Late Winter"
-            
+        self.calenderincrement = tasks.loop(seconds=5)(self.update_calendar)
+        self.IG_year = 0
+        self.IG_month = 0
+        self.IG_day = 0
+        self.IG_hour = 0
+        self.IG_minute = 0
+        self.season= None
+    i=True
+    async def calenderinit(self):
+        while True:
+            utc_now = datetime.now(timezone.utc)
+            skyblockstart = datetime.strptime("2019-06-11 17:55:00", "%Y-%m-%d %H:%M:%S").replace(tzinfo=timezone.utc)
+            secondssincestart = (utc_now - skyblockstart).total_seconds()
+            minutessincestart = int(secondssincestart // 60)
+
+            # Skyblock time constants
+            MINUTES_PER_IG_MINUTE = 0.01388
+            MINUTES_PER_IG_HOUR = 0.8333
+            MINUTES_PER_IG_DAY = 20
+            MINUTES_PER_IG_MONTH = 620
+            MINUTES_PER_IG_YEAR = 7440
+
+            IG_year, remainder = divmod(minutessincestart, MINUTES_PER_IG_YEAR)
+            IG_month, remainder = divmod(remainder, MINUTES_PER_IG_MONTH)
+            IG_day, remainder = divmod(remainder, MINUTES_PER_IG_DAY)
+            IG_hour, remainder = divmod(remainder, MINUTES_PER_IG_HOUR)
+            IG_minute, _ = divmod(remainder, MINUTES_PER_IG_MINUTE)
+
+            self.IG_year = IG_year + 1
+            self.IG_month = IG_month + 1
+            self.IG_day = IG_day + 1
+            self.IG_hour = int(IG_hour)
+            self.IG_minute = int(IG_minute)
+            self.IG_minutetenth = f"{self.IG_minute:02}"
+            if self.IG_minute % 5 == 0:
+                print(f"Initial IG time: {self.IG_year}-{self.IG_month}-{self.IG_day} {self.IG_hour}:{self.IG_minutetenth}")
+                if not self.calenderincrement.is_running():
+                    self.calenderincrement.start()
+                break
+            await asyncio.sleep(1)  # Check every second until IG minute % 5 == 0
+
+    async def update_calendar(self):
+        # Recalculate current IG time
+        utc_now = datetime.now(timezone.utc)
+        skyblockstart = datetime.strptime("2019-06-11 17:55:00", "%Y-%m-%d %H:%M:%S").replace(tzinfo=timezone.utc)
+        secondssincestart = (utc_now - skyblockstart).total_seconds()
+        minutessincestart = int(secondssincestart // 60)
+
+        MINUTES_PER_IG_MINUTE = 0.01388
+        MINUTES_PER_IG_HOUR = 0.8333
+        MINUTES_PER_IG_DAY = 20
+        MINUTES_PER_IG_MONTH = 620
+        MINUTES_PER_IG_YEAR = 7440
+
+        IG_year, remainder = divmod(minutessincestart, MINUTES_PER_IG_YEAR)
+        IG_month, remainder = divmod(remainder, MINUTES_PER_IG_MONTH)
+        IG_day, remainder = divmod(remainder, MINUTES_PER_IG_DAY)
+        IG_hour, remainder = divmod(remainder, MINUTES_PER_IG_HOUR)
+        IG_minute, _ = divmod(remainder, MINUTES_PER_IG_MINUTE)
+
+        self.IG_year = IG_year + 1
+        self.IG_month = IG_month + 1
+        self.IG_day = IG_day + 1
+        self.IG_hour = int(IG_hour)
+        self.IG_minute = int(IG_minute)
+        self.IG_minutetenth = f"{self.IG_minute:02}"
         if self.first== True:
             calanderembed=discord.Embed(title="Hypixel Calander", color=0x808080)
             calanderembed.set_thumbnail(url="attachment://ImportantTxtFiles/HypixelLogo.png")
@@ -106,50 +127,13 @@ class hypixel(commands.Cog):
             calanderembed.add_field(name="Current Major Events", value="CurrentEvents", inline=True)
             calanderembed.add_field(name="Major Events soon", value="EventsSoon", inline=True)
             await self.calendar_message.edit(embed=calanderembed)
-    i=True
-    def calenderinit(self):
-        global i
-        print("Loop started")
-        i=True
-        while i==True:
-            utc_now = datetime.now(timezone.utc)
-            skyblockstart = datetime.strptime("2019-06-11 17:55:00", "%Y-%m-%d %H:%M:%S").replace(tzinfo=timezone.utc)
-            #IRL time
-            secondssincestart = (utc_now - skyblockstart).total_seconds()
-            minutessincestart = int(secondssincestart// 60)
-            #IG time
-            # Skyblock time constants (in real minutes)
-            MINUTES_PER_IG_MINUTE = 0.01388      # 0.833 seconds
-            MINUTES_PER_IG_HOUR = 0.8333         # 50 seconds
-            MINUTES_PER_IG_DAY = 20
-            MINUTES_PER_IG_MONTH = 620
-            MINUTES_PER_IG_YEAR = 7440
-
-            # Compute each unit from the largest to the smallest
-            IG_year, remainder = divmod(minutessincestart, MINUTES_PER_IG_YEAR)
-            IG_month, remainder = divmod(remainder, MINUTES_PER_IG_MONTH)
-            IG_day, remainder = divmod(remainder, MINUTES_PER_IG_DAY)
-            IG_hour, remainder = divmod(remainder, MINUTES_PER_IG_HOUR)
-            IG_minute, _ = divmod(remainder, MINUTES_PER_IG_MINUTE)
-
-            # Convert to integers for display
-            self.IG_year = IG_year+1
-            self.IG_month = IG_month + 1   # +1 for human-readable months
-            self.IG_day = IG_day + 1       # +1 for human-readable days
-            self.IG_hour = int(IG_hour)
-            self.IG_minute = int(IG_minute)
-            if int(round(self.IG_minute)) % 5 == 0:
-                i=False
-                print("loop done")
     @commands.Cog.listener()
     async def on_ready(self):
         print("hypixel.py is ready")
         self.channel = self.bot.get_channel(1372924740993548360)
         await self.channel.purge()
-        (threading.Thread(target=self.calenderinit)).start()
-        if i== False:
-            await self.calenderincrement.start()
-            (threading.Thread(target=self.calenderinit)).join()
+        asyncio.create_task(self.calenderinit())
+
         
     
 
