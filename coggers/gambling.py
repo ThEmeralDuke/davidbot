c2451987ad120b6f4cd064792d08271b181bb6aa
ThEmeralDuke <mattytruesdale@gmail.com>
2025-06-08 21:14:00 +0100

    rings

--- coggers/gambling.py
+++ coggers/gambling.py
@@ -61,7 +61,9 @@ class gambling(commands.Cog):
                     lengthOfIteration = random.randint(0,(len(iteration)-1))
                     self.SlotRings[ring].append(iteration[lengthOfIteration])
                     
-            print(self.SlotRings)
+            print("/n".join(self.SlotRings[0]))
+            print("/n".join(self.SlotRings[1]))
+            print("/n".join(self.SlotRings[3]))
         except:
             await ctx.send("Please input a valid number to bet with")
 
