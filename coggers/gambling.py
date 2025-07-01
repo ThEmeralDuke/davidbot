914bced6f9d29edeee6b97e92b69d46b3cd6356a
ThEmeralDuke <mattytruesdale@gmail.com>
2025-07-01 10:23:07 +0100

    ./

--- coggers/gambling.py
+++ coggers/gambling.py
@@ -33,7 +33,7 @@ with open (filepath+"/ImportantTxtFiles/important.csv", "r") as info:
 info.close()
 
 # Load settings from the JSON file
-with open("config.json", "r") as f:
+with open("./config.json", "r") as f:
     SETTINGS = json.load(f)
 
 def cog_is_active():
