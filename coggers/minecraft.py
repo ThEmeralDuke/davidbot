a9671cfc5d1bfd2c9926c58efdd234716a28053e
ThEmeralDuke <mattytruesdale@gmail.com>
2025-07-15 18:30:41 +0100

    test

--- coggers/minecraft.py
+++ coggers/minecraft.py
@@ -138,7 +138,7 @@ class minecraft(commands.Cog):
             #    print("New day making")
                 #Creates a new folder for the backup if its a new day
                 print("attempting to create a filepath")
-                subprocess.run(["sudo","mkdir",Minecraftbackupfilepath])
+                #subprocess.run(["sudo","mkdir",Minecraftbackupfilepath])
                 print("filepath created")
 
             #    print("New day made")
@@ -156,13 +156,13 @@ class minecraft(commands.Cog):
                 #print("Hour joined")
                 #makes the backup under the hour and minute
                 print("attempting to create a filepath")
-                subprocess.run(["sudo","mkdir",backupfilepath])
+                #subprocess.run(["sudo","mkdir",backupfilepath])
                 print("filepath created")
                 #print("File made")
-                subprocess.run(["sudo","cp",Minecraftserverfilepath+"/world",backupfilepath+"/world/","-rf"])
+                #subprocess.run(["sudo","cp",Minecraftserverfilepath+"/world",backupfilepath+"/world/","-rf"])
                 #print("copied over the files")
                 #turns on automatic saving on the minecraft server as the backup is done
-                subprocess.run(["sudo","-u","server","tmux", "send-keys", "-t", "Minecraft", "/save-on", "ENTER"])
+                #subprocess.run(["sudo","-u","server","tmux", "send-keys", "-t", "Minecraft", "/save-on", "ENTER"])
                 await ctx.send("Minecraft backed up succesfully")
             else:
                 #print("Backup already exists so fuck you")
