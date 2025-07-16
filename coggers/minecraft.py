4663bd396d4775f797b7daf73934c90daa4ce4c9
ThEmeralDuke <mattytruesdale@gmail.com>
2025-07-16 22:03:33 +0100

    30

--- coggers/minecraft.py
+++ coggers/minecraft.py
@@ -82,7 +82,7 @@ class minecraft(commands.Cog):
                 subprocess.run(["sudo","-u","server","tmux", "send-keys", "-t", "Minecraft", "stop", "ENTER"])
                 subprocess.run(["sudo","-u","server","tmux", "send-keys", "-t", "Minecraft", "stop", "ENTER"])
                 await ctx.send("Minecraft shut down correctly and rebooting")
-                sleepyboi= 20
+                sleepyboi= 30
             except:
                 sleepyboi= 0
             time.sleep(sleepyboi) #Just give it more time to close
