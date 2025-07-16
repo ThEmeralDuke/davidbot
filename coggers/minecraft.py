d52fb42faa8aa60c5e76ea77e94477ea35c69c6a
ThEmeralDuke <mattytruesdale@gmail.com>
2025-07-16 21:59:57 +0100

    stoooop

--- coggers/minecraft.py
+++ coggers/minecraft.py
@@ -79,7 +79,8 @@ class minecraft(commands.Cog):
             try:
                 #sends the command to the tmux session
                 subprocess.run(["sudo","-u","server","tmux", "send-keys", "-t", "Minecraft", "ENTER"])
-                subprocess.run(["sudo","-u","server","tmux", "send-keys", "-t", "Minecraft", "/stop", "ENTER"])
+                subprocess.run(["sudo","-u","server","tmux", "send-keys", "-t", "Minecraft", "stop", "ENTER"])
+                subprocess.run(["sudo","-u","server","tmux", "send-keys", "-t", "Minecraft", "stop", "ENTER"])
                 sleepyboi= 7
             except:
                 sleepyboi= 0
@@ -127,7 +128,7 @@ class minecraft(commands.Cog):
             subprocess.run(["sudo","-u","server","tmux", "send-keys", "-t", "Minecraft", "/save-off", "ENTER"])
             #Spilitting time and becoming a time lord
 
-            day = str(time.strftime("%Y-%M-%D", time.localtime()))
+            day = str(time.strftime("%Y-%m-%D", time.localtime()))
             #print("Dating done")
             Minecraftbackupfilepath=("/opt/backups/minecraft/"+gametype+"/"+version) #Change this to the filepath of your minecraft server backups
             print("checking filepath")
