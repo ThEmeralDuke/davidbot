9c4d72a27718bb5f43cc876dfac2a4455f86e3b4
ThEmeralDuke <mattytruesdale@gmail.com>
2025-09-25 00:15:08 +0100

    spaced it out

--- coggers/minecraft.py
+++ coggers/minecraft.py
@@ -88,7 +88,7 @@ class minecraft(commands.Cog):
             time.sleep(sleepyboi) #Just give it more time to close
             subprocess.run(["sudo", "-u", "server", "/bin/bash", "/home/server/sh/mcstart.sh"])
             time.sleep(35) #Give it time to start
-            subprocess.run(['sudo', '-u', ',server', 'ssh', '-i', '~/.ssh/ssh*', 'ubuntu@132.145.78.199', '"mkdir test"'])
+            subprocess.run(['sudo', '-u', 'server', 'ssh', '-i', '/home/server/.ssh/ssh-key-2025-09-15.key', 'ubuntu@132.145.78.199', 'mkdir', 'test'])
             await ctx.send("Minecraft rebooted. please wait for the proxy to turn on")
         except:
             #If there is an error, log it and tell the user
