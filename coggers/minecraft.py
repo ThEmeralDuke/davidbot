7972f4c353675530cb5d26f53a9c904c5ec21f83
ThEmeralDuke <mattytruesdale@gmail.com>
2025-09-24 23:36:59 +0100

    reboot might be set up!

--- coggers/minecraft.py
+++ coggers/minecraft.py
@@ -53,8 +53,8 @@ settings.close()
 
 ##Minecraft settings##
 
-Minecraftserverfilepath="/opt/minecraft" #Change this to the filepath of your minecraft server
-Minecraftbackupfilepath="/opt/backups/minecraft/"+gametype+"/"+version+"/" #Change this to the filepath of your minecraft server backups
+Minecraftserverfilepath="/home/server/Minecraft" #Change this to the filepath of your minecraft server
+#Minecraftbackupfilepath="/opt/backups/minecraft/"+gametype+"/"+version+"/" #Change this to the filepath of your minecraft server backups
 class minecraft(commands.Cog):
     def __init__(self, bot):
         self.bot= bot
@@ -80,14 +80,16 @@ class minecraft(commands.Cog):
                 #sends the command to the tmux session
                 subprocess.run(["sudo","-u","server","tmux", "send-keys", "-t", "Minecraft", "ENTER"])
                 subprocess.run(["sudo","-u","server","tmux", "send-keys", "-t", "Minecraft", "stop", "ENTER"])
-                subprocess.run(["sudo","-u","server","tmux", "send-keys", "-t", "Minecraft", "/stop", "ENTER"])
+                #subprocess.run(["sudo","-u","server","tmux", "send-keys", "-t", "Minecraft", "/stop", "ENTER"])
                 await ctx.send("Minecraft shut down correctly and rebooting")
                 sleepyboi= 30
             except:
                 sleepyboi= 0
             time.sleep(sleepyboi) #Just give it more time to close
             subprocess.run(["sudo", "-u", "server", "/bin/bash", "/home/server/sh/mcstart.sh"])
-            
+            time.sleep(60) #Give it time to start
+            subprocess.run(['sudo -u server ssh -i ~/.ssh/ssh* ubuntu@132.145.78.199 "sudo reboot"'])
+            await ctx.send("Minecraft rebooted. please wait for the proxy to turn on")
         except:
             #If there is an error, log it and tell the user
             Level= "Severe"
@@ -110,92 +112,92 @@ class minecraft(commands.Cog):
 
 
     #This creates a backup of the minecraft server
-    @commands.command(pass_context=True)
-    @commands.has_role(Adminrole)
-    async def MCbackup(self ,ctx):
+    #@commands.command(pass_context=True)
+    #@commands.has_role(Adminrole)
+    #async def MCbackup(self ,ctx):
 
-        global person
-        person= ctx.author
-        person= str(person)
-        print("Minecraft Backedup by "+ person)
-        await ctx.send("Backing up Minecraft...")
-        with open (Generallog, "a") as log:
-            currenttime= str(time.strftime("%Y-%m-%D %H:%M:%S", time.localtime()))
-            log.write(currenttime+ "   Minecraft Backed up by "+ person+"\n")
-        log.close
-        try:
-            #more MC commands for the tmux session
-            subprocess.run(["sudo","-u","server","tmux", "send-keys", "-t", "Minecraft", "ENTER"])
-            #turns off automatic saving on the minecraft server so the backup is not corrupted
-            subprocess.run(["sudo","-u","server","tmux", "send-keys", "-t", "Minecraft", "/save-off", "ENTER"])
-            #Spilitting time and becoming a time lord
-
-            day = str(time.strftime("%Y-%m-%D", time.localtime()))
+    #    global person
+     #   person= ctx.author
+      #  person= str(person)
+    #    print("Minecraft Backedup by "+ person)
+    #    await ctx.send("Backing up Minecraft...")
+    #    with open (Generallog, "a") as log:
+    #        currenttime= str(time.strftime("%Y-%m-%D %H:%M:%S", time.localtime()))
+    #        log.write(currenttime+ "   Minecraft Backed up by "+ person+"\n")
+    #    log.close
+    #    try:
+    #        #more MC commands for the tmux session
+    #        subprocess.run(["sudo","-u","server","tmux", "send-keys", "-t", "Minecraft", "ENTER"])
+    #        #turns off automatic saving on the minecraft server so the backup is not corrupted
+    #        subprocess.run(["sudo","-u","server","tmux", "send-keys", "-t", "Minecraft", "/save-off", "ENTER"])
+    #        #Spilitting time and becoming a time lord
+#
+     #       day = str(time.strftime("%Y-%m-%D", time.localtime()))
             #print("Dating done")
-            Minecraftbackupfilepath=("/opt/backups/minecraft/"+gametype+"/"+version) #Change this to the filepath of your minecraft server backups
-            print("checking filepath")
-            backupfile_exists = os.path.isdir(Minecraftbackupfilepath+"/"+day)
-            print(backupfile_exists)
-            Minecraftbackupfilepath= os.path.join(Minecraftbackupfilepath,day)
-            if backupfile_exists== False:
+     #       Minecraftbackupfilepath=("/opt/backups/minecraft/"+gametype+"/"+version) #Change this to the filepath of your minecraft server backups
+     #       print("checking filepath")
+    #        backupfile_exists = os.path.isdir(Minecraftbackupfilepath+"/"+day)
+    #        print(backupfile_exists)
+    #        Minecraftbackupfilepath= os.path.join(Minecraftbackupfilepath,day)
+    #        if backupfile_exists== False:
             #    print("New day making")
-                #Creates a new folder for the backup if its a new day
-                print("attempting to create a filepath")
+    #            #Creates a new folder for the backup if its a new day
+    #            print("attempting to create a filepath")
                 #subprocess.run(["sudo","mkdir",Minecraftbackupfilepath])
-                print("filepath created")
+   #             print("filepath created")
 
             #    print("New day made")
             #print("Datechecked/made")
             #More time lord stuff
-            hour= str(time.strftime("%H", time.localtime()))
-            print(hour)
+    #        hour= str(time.strftime("%H", time.localtime()))
+     #       print(hour)
             #print("Hours calculated")
-            backupfilepath= os.path.join(Minecraftbackupfilepath,hour)
-            print("checking filepath")
-            backupfile_exists = os.path.isdir(backupfilepath)
-            print(backupfile_exists)
+    #        backupfilepath= os.path.join(Minecraftbackupfilepath,hour)
+    #        print("checking filepath")
+    #        backupfile_exists = os.path.isdir(backupfilepath)
+    #        print(backupfile_exists)
             #print("Hours checked")
-            if backupfile_exists== False:
-                Minecraftbackupfilepath= os.path.join(Minecraftbackupfilepath,hour)
+    #        if backupfile_exists== False:
+    #            Minecraftbackupfilepath= os.path.join(Minecraftbackupfilepath,hour)
                 #print("Hour joined")
                 #makes the backup under the hour and minute
-                print("attempting to create a filepath")
-                #subprocess.run(["sudo","mkdir",backupfilepath])
-                print("filepath created")
+     #           print("attempting to create a filepath")
+     ##           #subprocess.run(["sudo","mkdir",backupfilepath])
+     #           print("filepath created")
                 #print("File made")
-                #subprocess.run(["sudo","cp",Minecraftserverfilepath+"/world",backupfilepath+"/world/","-rf"])
+    #            #subprocess.run(["sudo","cp",Minecraftserverfilepath+"/world",backupfilepath+"/world/","-rf"])
                 #print("copied over the files")
                 #turns on automatic saving on the minecraft server as the backup is done
                 #subprocess.run(["sudo","-u","server","tmux", "send-keys", "-t", "Minecraft", "/save-on", "ENTER"])
-                await ctx.send("Minecraft backed up succesfully")
-                print(Minecraftbackupfilepath,backupfilepath)
-            else:
+     #           await ctx.send("Minecraft backed up succesfully")
+     #           print(Minecraftbackupfilepath,backupfilepath)
+     #       else:
                 #print("Backup already exists so fuck you")
                 #Boo-hoo the backup already existed in that minute so why the fuck are you making another
-                subprocess.run(["sudo","-u","server","tmux", "send-keys", "-t", "Minecraft", "/save-on", "ENTER"])
-        except:
+     #           subprocess.run(["sudo","-u","server","tmux", "send-keys", "-t", "Minecraft", "/save-on", "ENTER"])
+     #   except:
             #error catching and logging
-            Level= "Severe"
-            Reason= "Minecraft failed to Backup"
-            await ctx.send("Minecraft Failed to Backup")
-            LogError(Level,Reason)
-            #turns on saving so it doesnt stay off if there is an error
-            subprocess.run(["sudo","-u","server","tmux", "send-keys", "-t", "Minecraft", "/save-on", "ENTER"])
-            pass
+     #       Level= "Severe"
+     #       Reason= "Minecraft failed to Backup"
+     #       await ctx.send("Minecraft Failed to Backup")
+     #       LogError(Level,Reason)
+     #       #turns on saving so it doesnt stay off if there is an error
+      #      subprocess.run(["sudo","-u","server","tmux", "send-keys", "-t", "Minecraft", "/save-on", "ENTER"])
+     #       pass
         pass
     #Incase someone doesnt have permissions
-    @MCbackup.error
-    async def MCbackupError(self ,ctx ,error):
-        global person
-        if isinstance(error, commands.CheckFailure):
-            person= ctx.author
-            personID= person.id
-            person= str(person)
-            personID= str(personID)
-            Level= "Warn"
-            Reason= ("Unauthorised Minecraft Backup attempted by",person)
-            await ctx.send("You dont have permissions ("+Adminrole+") to do this <@"+personID+">")
-            LogError(Level,Reason)
+    #@MCbackup.error
+    #async def MCbackupError(self ,ctx ,error):
+    #    global person
+    #    if isinstance(error, commands.CheckFailure):
+    #        person= ctx.author
+    #        personID= person.id
+    #        person= str(person)
+    #        personID= str(personID)
+    #        Level= "Warn"
+    #        Reason= ("Unauthorised Minecraft Backup attempted by",person)
+    #        await ctx.send("You dont have permissions ("+Adminrole+") to do this <@"+personID+">")
+    #        LogError(Level,Reason)
 
 async def setup(bot):
       await bot.add_cog(minecraft(bot))
