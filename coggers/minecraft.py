3cba8eae8dec281608381db59c30dfb7eb80a794
ThEmeralDuke <mattytruesdale@gmail.com>
2025-09-27 16:42:22 +0100

    oops

--- coggers/minecraft.py
+++ coggers/minecraft.py
@@ -110,7 +110,7 @@ class minecraft(commands.Cog):
                 await asyncio.sleep(sleepyboi) #Just give it more time to close
                 subprocess.run(["sudo", "-u", "server", "/bin/bash", "/home/server/sh/mcstart.sh"])
                 await asyncio.sleep(35) #Give it time to start
-                #subprocess.run(["sudo", "-u", "server", "ssh", "-i", "/home/server/.ssh/ssh-key-2025-09-15.key", "ubuntu@132.145.78.199", "sudo", "reboot"])
+                subprocess.run(["sudo", "-u", "server", "ssh", "-i", "/home/server/.ssh/ssh-key-2025-09-15.key", "ubuntu@132.145.78.199", "sudo", "reboot"])
 
 
                 await ctx.send("Minecraft rebooted. please wait for the proxy to turn on")
