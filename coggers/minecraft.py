7406663daa5408b427731b3bbd71305881da6be8
ThEmeralDuke <mattytruesdale@gmail.com>
2025-09-27 13:31:34 +0100

    please dont make me split them

--- coggers/minecraft.py
+++ coggers/minecraft.py
@@ -79,6 +79,11 @@ class minecraft(commands.Cog):
             try:
                 #sends the command to the tmux session
                 subprocess.run(["sudo","-u","server","tmux", "send-keys", "-t", "Minecraft", "ENTER"])
+                subprocess.run(["sudo","-u","server","tmux", "send-keys", "-t", "Minecraft", ,"/say", "Server reboot in 5 minutes." "ENTER"])
+                time.sleep(240) #Wait 5 minutes
+                subprocess.run(["sudo","-u","server","tmux", "send-keys", "-t", "Minecraft", "ENTER"])
+                subprocess.run(["sudo","-u","server","tmux", "send-keys", "-t", "Minecraft", ,"/say", "Server reboot in 1 minute. Get to a safe place and finish up what you are doing" "ENTER"])
+                time.sleep(60) #Wait 5 minutes
                 subprocess.run(["sudo","-u","server","tmux", "send-keys", "-t", "Minecraft", "stop", "ENTER"])
                 #subprocess.run(["sudo","-u","server","tmux", "send-keys", "-t", "Minecraft", "/stop", "ENTER"])
                 await ctx.send("Minecraft shut down correctly and rebooting")
