092b603ff70199b19f00d1b0d8b71f16213ae4e5
ThEmeralDuke <mattytruesdale@gmail.com>
2025-09-27 16:17:30 +0100

    DUH

--- coggers/minecraft.py
+++ coggers/minecraft.py
@@ -79,7 +79,7 @@ class minecraft(commands.Cog):
                 except:
                     await ctx.send("Please enter the time in digits. Defaulting to 5 minutes")
                     resettime= "5"
-            resettimeint= int(resettime)
+            resettimeint= int(resettime)*60
             resettime= str(time)
             print("Minecraft reboot triggered by "+ person)
             await ctx.send("Restarting Minecraft in "+resettime+" minute(s).")
