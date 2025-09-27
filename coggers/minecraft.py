dc35773f1cbdcaf11aa690ad6f2822f3b648d06a
ThEmeralDuke <mattytruesdale@gmail.com>
2025-09-27 13:39:27 +0100

    oh.

--- coggers/minecraft.py
+++ coggers/minecraft.py
@@ -79,7 +79,7 @@ class minecraft(commands.Cog):
             try:
                 #sends the command to the tmux session
                 subprocess.run(["sudo","-u","server","tmux", "send-keys", "-t", "Minecraft", "ENTER"])
-                subprocess.run(["sudo","-u","server","tmux", "send-keys", "-t", "Minecraft", ,"/say", "Server", "reboot", "in", "5", "minutes.", "ENTER"])
+                subprocess.run(["sudo","-u","server","tmux", "send-keys", "-t", "Minecraft", "/say", "Server", "reboot", "in", "5", "minutes.", "ENTER"])
                 time.sleep(240) #Wait 5 minutes
                 subprocess.run(["sudo","-u","server","tmux", "send-keys", "-t", "Minecraft", "ENTER"])
                 subprocess.run(["sudo","-u","server","tmux", "send-keys", "-t", "Minecraft", ,"/say", "Server", "reboot", "in", "1", "minute.", "Get", "to", "a", "safe", "place", "and", "finish", "up", "what", "you", "are", "doing", "ENTER"])
