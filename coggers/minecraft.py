0f2dbd79cc751b2a6f4ccf2d16e4132fcbcb2a59
ThEmeralDuke <mattytruesdale@gmail.com>
2025-10-03 00:48:38 +0100

    one minute min

--- coggers/minecraft.py
+++ coggers/minecraft.py
@@ -83,6 +83,9 @@ class minecraft(commands.Cog):
             self.resettimeint= int(resettime)
             self.resettimeint= self.resettimeint*60
             resettime= str(resettime)
+            if (self.resettimeint-60)<=0:
+                resettime = 1
+                self.resettimeint = 60
             print("Minecraft reboot triggered by "+ person)
             await ctx.send("Restarting Minecraft in "+resettime+" minute(s).")
             with open (Generallog, "a") as log:
